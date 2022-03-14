ef53c3e298d57d0d2c32cec409c32982cc8c16ce
Michael-Aga <MichaelAgarkov@gmail.com>
2022-03-14 17:46:05 +0200

    last changes

--- Main.cpp
+++ Main.cpp
@@ -7,7 +7,7 @@ int main()
 {
     int col, row;
     char first_shape, second_shape;
-    cout << "Enter 2 positive and odd numbers, then enter 2 symbols for the mat" << endl;
+    cout << "Enter 2 positive and odd numbers first is col second is row, then enter 2 symbols for the mat" << endl;
     cin >> col >> row >> first_shape >> second_shape;
     ariel::mat(col, row, first_shape, second_shape);
     return 1;
